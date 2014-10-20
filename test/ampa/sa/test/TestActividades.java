0df1867b71264130fbd090a8dba53553302a86c2
Darktuse <Darktuse@Darktuse-PC>
2014-10-20 12:07:29 +0200

    Test para las actividades

--- test/ampa/sa/test/TestActividades.java
+++ test/ampa/sa/test/TestActividades.java
@@ -1,14 +1,152 @@
 package ampa.sa.test;
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
+
 import org.junit.Test;
+
+import ampa.sa.activity.Activity;
+import ampa.sa.activity.ActivityService;
+import ampa.sa.student.Student;
+import ampa.sa.util.exceptions.DuplicateInstanceException;
+import ampa.sa.util.exceptions.InstanceNotFoundException;
 
 
 public class TestActividades {
 
+	private ActivityService activityService = ActivityService.getInstance();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addActivity() {
+
+		ActivityService activityService = ActivityService.getInstance();
+		Activity activity = new Activity(4, "Baloncesto", 10, new BigDecimal(
+				20.0), new HashSet<Student>());
+
+		try {
+			activityService.create(activity);
+		} catch (DuplicateInstanceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			assertEquals(activityService.find(4), activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test(expected = DuplicateInstanceException.class)
+	public void addDuplicateActivity() {
+
+		ActivityService activityService = ActivityService.getInstance();
+		Activity activity = new Activity(4, "Baloncesto", 10, new BigDecimal(
+				20.0), new HashSet<Student>());
+
+		try {
+			activityService.create(activity);
+			activityService.create(activity);
+		} catch (DuplicateInstanceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test(expected = InstanceNotFoundException.class)
+	public void removeActivity() {
+
+		ActivityService activityService = ActivityService.getInstance();
+		Activity activity = new Activity(4, "Baloncesto", 10, new BigDecimal(
+				20.0), new HashSet<Student>());
+
+		try {
+			activityService.create(activity);
+		} catch (DuplicateInstanceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			assertEquals(activityService.find(4), activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			activityService.remove(activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			assertEquals(activityService.find(4), activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test(expected = InstanceNotFoundException.class)
+	public void removeInexistentActivity() {
+
+		ActivityService activityService = ActivityService.getInstance();
+		Activity activity = new Activity(4, "Baloncesto", 10, new BigDecimal(
+				20.0), new HashSet<Student>());
+
+		try {
+			activityService.remove(activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void updateActivity() {
+
+		ActivityService activityService = ActivityService.getInstance();
+		Activity activity = new Activity(4, "Baloncesto", 10, new BigDecimal(
+				20.0), new HashSet<Student>());
+
+		try {
+			activityService.create(activity);
+		} catch (DuplicateInstanceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		activity.setName("Baloncesto Primaria");
+
+		try {
+			activityService.update(activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void findInexistantActivity() {
+
+		ActivityService activityService = ActivityService.getInstance();
+		Activity activity = new Activity(4, "Baloncesto", 10, new BigDecimal(
+				20.0), new HashSet<Student>());
+
+		try {
+			assertEquals(activityService.find(4), activity);
+		} catch (InstanceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
