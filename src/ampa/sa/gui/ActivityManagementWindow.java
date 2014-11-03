073fabd009e176392a2d2f5987aa588b5ad33950
AlejandroFortes <alejandro.fortes.lopes@gmail.com>
2014-11-03 19:55:04 +0100

    Creada ventana para la matricula en actividades

---
+++ src/ampa/sa/gui/ActivityManagementWindow.java
@@ -0,0 +1,43 @@
+package ampa.sa.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import ampa.sa.student.Student;
+
+public class ActivityManagementWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ActivityManagementWindow frame = new ActivityManagementWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public ActivityManagementWindow(Student student) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
