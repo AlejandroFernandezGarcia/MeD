87942674af5a5ed9f3987202303b0a8ac7c66188
alejandro <Alejandro@10.20.154.236>
2014-11-07 00:17:56 +0100

    Mostrar historico de recibos aún no completado

--- src/ampa/sa/gui/BillHistoryWindow.java
+++ src/ampa/sa/gui/BillHistoryWindow.java
@@ -1,5 +1,6 @@
 package ampa.sa.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.math.BigDecimal;
 import java.util.Calendar;
@@ -33,6 +34,7 @@ public class BillHistoryWindow extends JFrame {
 	private JTable receiptTable;
 	private Household household;
 	private ReceiptService receiptService;
+	private JTable tableLinesBill;
 
 	/**
 	 * Launch the application.
@@ -59,8 +61,12 @@ public class BillHistoryWindow extends JFrame {
 		setContentPane(contentPane);
 
 		JPanel leftPanel = new JPanel();
-
+		contentPane.add(leftPanel);
+		leftPanel.setLayout(new BorderLayout(0, 0));
+		
 		JPanel rigthPanel = new JPanel();
+		contentPane.add(rigthPanel);
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(
 				Alignment.TRAILING).addGroup(
@@ -81,6 +87,8 @@ public class BillHistoryWindow extends JFrame {
 						Short.MAX_VALUE));
 
 		JScrollPane scrollPane = new JScrollPane();
+		leftPanel.add(scrollPane);
+		
 		GroupLayout gl_leftPanel = new GroupLayout(leftPanel);
 		gl_leftPanel.setHorizontalGroup(gl_leftPanel.createParallelGroup(
 				Alignment.LEADING).addComponent(scrollPane,
@@ -122,22 +130,59 @@ public class BillHistoryWindow extends JFrame {
 		lblHouseHold.setHorizontalTextPosition(SwingConstants.CENTER);
 		lblHouseHold.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHouseHold.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		JPanel explainPanel = new JPanel();
 		GroupLayout gl_rigthPanel = new GroupLayout(rigthPanel);
 		gl_rigthPanel.setHorizontalGroup(
 			gl_rigthPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_rigthPanel.createSequentialGroup()
-					.addGap(10)
-					.addComponent(lblHouseHold)
-					.addContainerGap(77, Short.MAX_VALUE))
+					.addGroup(gl_rigthPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_rigthPanel.createSequentialGroup()
+							.addGap(10)
+							.addComponent(lblHouseHold))
+						.addComponent(explainPanel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE))
+					.addContainerGap())
 		);
 		gl_rigthPanel.setVerticalGroup(
 			gl_rigthPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_rigthPanel.createSequentialGroup()
 					.addGap(11)
 					.addComponent(lblHouseHold, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(213, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(explainPanel, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GroupLayout gl_explainPanel = new GroupLayout(explainPanel);
+		gl_explainPanel.setHorizontalGroup(
+			gl_explainPanel.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 208, GroupLayout.PREFERRED_SIZE)
+		);
+		gl_explainPanel.setVerticalGroup(
+			gl_explainPanel.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
+		);
+		
+		tableLinesBill = new JTable();
+		tableLinesBill.setModel(new DefaultTableModel(new Object[][] { { null,
+			null }, }, new String[] { "Concepto", "Precio" }) {
+			Class[] columnTypes = new Class[] { String.class, BigDecimal.class };
+
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+
+			boolean[] columnEditables = new boolean[] { false, false };
+
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane_1.setViewportView(tableLinesBill);
+		explainPanel.setLayout(gl_explainPanel);
 		rigthPanel.setLayout(gl_rigthPanel);
+		fillRightPanel(rigthPanel);
 
 		receiptTable.getSelectionModel().addListSelectionListener(
 				new ListSelectionListener() {
@@ -174,6 +219,25 @@ public class BillHistoryWindow extends JFrame {
 	}
 
 	private void fillRightPanel(JPanel panel) throws ReceiptsNotFoundException {
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		tableLinesBill = new JTable();
+		tableLinesBill.setModel(new DefaultTableModel(new Object[][] { { null,
+			null }, }, new String[] { "Concepto", "Precio" }) {
+			Class[] columnTypes = new Class[] { String.class, BigDecimal.class };
+
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+
+			boolean[] columnEditables = new boolean[] { false, false };
+
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane_1.setViewportView(tableLinesBill);
+		
 		if (receiptTable.getRowCount() == 0) {
 			return;
 		}
@@ -182,9 +246,18 @@ public class BillHistoryWindow extends JFrame {
 		final Receipt receiptSelected = receipts.get(receiptTable
 				.getSelectedRow());
 		Set<ReceiptLine> receiptLines = receiptSelected.getReceiptLines();
-		Component[] componentsPanel = panel.getComponents();
-		for (Component component : componentsPanel) {
-
+		DefaultTableModel dtm = (DefaultTableModel) tableLinesBill.getModel();
+		for (int i = 0; i < dtm.getRowCount(); i++) {
+			dtm.removeRow(i);
 		}
+		
+		for (ReceiptLine receiptLine : receiptLines) {
+			Object[] data = {receiptLine.getConcept(), receiptLine.getPrice()};
+			dtm.addRow(data);
+		}
+		
+		tableLinesBill.updateUI();// DUDA Es necesario?
+		
 	}
+	
 }
